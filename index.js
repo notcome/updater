ef232215182bb7077e6173516441a986d3d5545d
notcome <notcome@me.com>
2014-04-04 21:44:07 +0800

    fs -> stdio

--- index.js
+++ index.js
@@ -2,21 +2,18 @@ var githubhook = require('githubhook');
 var console = require('console');
 var github = githubhook({port: 8000});
 var exec = require('child_process').exec;
-var fs = require('fs');
 
 var repos = {
   'Selveskii.me': '/root/git_test/Selveskii.me'
 }
 
-var log = fs.createWriteStream('log', { flags: 'a', encoding: 'utf8' });
-
 function pullGenerator (reponame) {
   var repopath = repos[reponame];
   return function (event, ref) {
     exec(__dirname + '/git-pull ' + repopath, function (err, out, err) {
-      if (err) fs.write('exec error:\n' + err);
-      fs.write(reponame, event, ref);
-      fs.write(out);
+      if (err) console.error('exec error:\n' + err);
+      console.log(reponame, event, ref);
+      console.log(out);
     });
   }
 }
