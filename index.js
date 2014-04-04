33d1d565efb579ffaa0df8fc492372f9cd5ecfb7
notcome <notcome@me.com>
2014-04-04 21:38:45 +0800

    prototype

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 var githubhook = require('githubhook');
 var console = require('console');
 var github = githubhook({port: 8000});
+var exec = require('child_process').exec
+
+var repos = {
+  'Selveskii.me': '/root/git_test/Selveskii.me'
+}
+
+var log = fs.createWriteStream('log', { flags: 'a', encoding: 'utf8' });
+
+function pullGenerator (reponame) {
+  var repopath = repos[reponame];
+  return function (event, ref) {
+    exec(__dirname + '/git-pull ' + repopath, function (err, out, err) {
+      if (err) fs.write('exec error:\n' + err);
+      fs.write(reponame, event, ref);
+      fs.write(out);
+    });
+  }
+}
 
 github.listen();
 
-github.on('Selveskii.me', function (event, ref, data) {
-  console.log('Selveskii.me');
-  console.log('[EVENT]');
-  console.log(event);
-  console.log('[REF]');
-  console.log(ref);
-  console.log('[DATA]');
-  console.log(data);
-});
+for (var reponame in repos)
+  github.on(reponame, pullGenerator(reponame));
